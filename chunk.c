07bf21809dfd042d44eeef70c6211d575899f3ee
Sterling Jeppson <sterlingijeppson@gmail.com>
2023-01-21 22:10:19 -0800

    Initial commit

---
+++ chunk.c
@@ -0,0 +1,14 @@
+#include "chunk.h"
+
+void initChunk(Chunk* chunk) {
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+    if(chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+    }
+}
