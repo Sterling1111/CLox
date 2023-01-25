72fe0e1d3c75b4ace0de1221ed2d4912e8d5f345
Sterling1111 <sterlingijeppson@gmail.com>
2023-01-24 20:59:34 -0800

    finished chapter on virtual machine

--- chunk.h
+++ chunk.h
@@ -3,19 +3,29 @@
 
 #include "common.h"
 #include "memory.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
+    OP_NEGATE,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
 } OpCode;
 
 typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 void freeChunk(Chunk* chunk);
+int addConstant(Chunk* chunk, Value value);
 
 #endif //CLOX_CHUNK_H
--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include "chunk.h"
+#include "debug.h"
+#include "vm.h"
+
 
 int main(int argc, const char* argv[]) {
-    printf("Hello, World!\n");
+    initVM();
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    interpret(&chunk);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    freeVM();
     return 0;
 }
